7cd39f54edb65b69893788c70c9bf62dc625b3a2
YongWookHa <ywha12@gmail.com>
2021-05-07 04:52:21 +0000

    fix monitor and use gpu

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import argparse
+import torch
 import pytorch_lightning as pl
 from pytorch_lightning.accelerators import accelerator
 from pytorch_lightning.loggers import TensorBoardLogger
@@ -19,9 +20,10 @@ if __name__ == "__main__":
                         help='learning rate for training')
     parser.add_argument('-e', '--max_epoch', type=int, default=100,
                         help='max epoch')
-    
+
     args = parser.parse_args()
-    
+    cfg.lr = args.learning_rate
+
     if args.module == 'detector':
         from models.craft_pl import CRAFT
         from datasets.craft_dataset import DataLoaderSYNTH
@@ -30,23 +32,25 @@ if __name__ == "__main__":
     elif args.module == 'recognizer':
         from models.deepTextRecog_pl import DeepTextRecog
         model = DeepTextRecog(cfg)
-    
+
     trainSize = int(len(dataset)*0.9)
     trainDataset, validDataset = random_split(dataset, [trainSize, len(dataset)-trainSize])
     trainDataloader = DataLoader(trainDataset, batch_size=args.batch_size, num_workers=16)
     validDataloader = DataLoader(validDataset, batch_size=args.batch_size, num_workers=16)
-    
+
     logger = TensorBoardLogger('tb_logs', name=args.module, version=args.version)
     # lr_callback = pl.callbacks.LearningRateMonitor(logging_interval='step')
     ckpt_callback = pl.callbacks.ModelCheckpoint(
-        monitor=['val_loss', 'val_fscore'],
+        monitor='fscore',
         dirpath=f'checkpoints/version_{args.version}',
-        filename='checkpoints-{epoch:02d}-{val_acc:.2f}',
+        filename='checkpoints-{epoch:02d}-{val_fscore:.2f}',
         save_top_k=3,
         mode='max',
     )
 
-    trainer = pl.Trainer(gpus=0, max_epochs=args.max_epoch, logger=logger, num_sanity_val_steps=1,
+    n_gpu = torch.cuda.device_count()
+
+    trainer = pl.Trainer(gpus=n_gpu, max_epochs=args.max_epoch, logger=logger, num_sanity_val_steps=1,
                          accelerator='ddp', callbacks=[ckpt_callback])
-                        
+
     trainer.fit(model, train_dataloader=trainDataloader, val_dataloaders=validDataloader)
